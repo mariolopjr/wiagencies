5d690e45ea5b0a678d6c407f23a3ebbbb7a94875
Mario <mario@techmunchies.net>
2019-06-11 20:24:45 -0400

    Fixed Google Maps iframe size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ class Navbar extends React.Component {
                 width="100%"
                 height="100%"
                 frameBorder="0"
-                style={{ border: 0 }}
+                style={{ border: 0, height: "100%", width: "100%" }}
                 allowFullScreen
               ></iframe>
             </section>
